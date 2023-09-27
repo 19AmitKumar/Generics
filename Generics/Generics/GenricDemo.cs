f75d4c2d41c24409e28a9a3c2e86b0c2d6a467f6
19AmitKumar <ak9978292@gmail.com>
2023-09-27 09:02:23 +0530

    [Amit] Add:Examples of Generics

---
+++ Generics/Generics/GenricDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    //Generics means single form
+    //It doesnot specify a particular Datatype
+    //Generic allow us asingle class or method that can be used to different datatypes
+    //<Type> with Type Name
+    //Type parameter
+    internal class GenricDemo <T>
+    {
+        //public string data;
+        public T data;
+        public GenricDemo(T data)
+        {
+            this.data = data;
+            Console.WriteLine(this.data);
+
+
+        }
+        public void DemoMethod(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+    public class GenericOne
+    {
+        public void GenericOneMethod<T>(T info) 
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenricDemo<int> genric = new GenricDemo<int>(10);
+            GenricDemo<string> genri = new GenricDemo<string>("Amit");
+            GenricDemo<char> genr = new GenricDemo<char>('a');
+            GenricDemo<bool> gen = new GenricDemo<bool>(false);
+            Console.WriteLine();
+            genric.DemoMethod(10);
+            genri.DemoMethod("satyam");
+            genr.DemoMethod('s');
+            gen.DemoMethod(true);
+            Console.WriteLine();
+            GenericOne one = new GenericOne();
+            one.GenericOneMethod(1);
+            one.GenericOneMethod(10.9);
+            one.GenericOneMethod("Amit");
+            one.GenericOneMethod(false);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+    
+}
